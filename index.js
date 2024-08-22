8ce29cd0415c91891babec5682592deb072bb02d
Rv waqas <145973183+Rvwaqas@users.noreply.github.com>
2024-08-22 23:11:02 +0500

    Add files via upload
    add

---
+++ index.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Generate secrete number
+const secretNumber = Math.floor((Math.random() * 2)) + 1;
+console.log(secretNumber);
+let userInput = await inquirer.prompt({
+    type: "list",
+    name: "num",
+    message: "Please picked one number",
+    choices: ["1", "2"]
+});
+//convert input in number
+let num1 = parseInt(userInput.num);
+switch (true) {
+    case num1 === secretNumber:
+        console.log(chalk.bold.italic("You Won!"));
+        break;
+    default:
+        console.log("try again!");
+}
